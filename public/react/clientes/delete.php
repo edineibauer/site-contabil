85ad9b03254c5214e8e1677f3565747401c2bfd6
Edinei <galera.org@gmail.com>
2019-02-15 16:03:40 -0200

    delete user call react

--- public/react/clientes/delete.php
+++ public/react/clientes/delete.php
@@ -3,7 +3,9 @@ if (!empty($dados['usuario_id']) && is_numeric($dados['usuario_id'])) {
     $read = new \Conn\Read();
     $read->exeRead("usuarios", "WHERE id = :id", "id={$dados['usuario_id']}");
     if($read->getResult()) {
+        $user = $read->getResult()[0];
         $del = new \Conn\Delete();
         $del->exeDelete("usuarios", "WHERE id = :id", "id={$dados['usuario_id']}");
+        new \Entity\React("delete", "usuarios", $user, $user);
     }
 }
